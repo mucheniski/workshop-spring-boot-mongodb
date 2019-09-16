ef0f7355ec559fca29f7aec06ed9355380ea15b9
Diego Mucheniski <mucheniski@gmail.com>
2019-09-16 20:46:37 -0300

    322 Insert de usuario com POST

--- src/main/java/com/mucheniski/workshopmongo/services/UserService.java
+++ src/main/java/com/mucheniski/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mucheniski.workshopmongo.domains.User;
+import com.mucheniski.workshopmongo.dto.UserDTO;
 import com.mucheniski.workshopmongo.exception.ObjectNotFoundException;
 import com.mucheniski.workshopmongo.repository.UserRepository;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado para o id: " + id));		
+	}
+	
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 	
 }
