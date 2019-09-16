e743b6bcf41838054a56891489e468a6e70f8180
Diego Mucheniski <mucheniski@gmail.com>
2019-09-16 20:15:26 -0300

    Usando padrao DTO para retornar usuarios e obtendo por id

--- src/main/java/com/mucheniski/workshopmongo/resources/UserResource.java
+++ src/main/java/com/mucheniski/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.mucheniski.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mucheniski.workshopmongo.domains.User;
+import com.mucheniski.workshopmongo.dto.UserDTO;
 import com.mucheniski.workshopmongo.services.UserService;
 
 @RestController
@@ -19,9 +22,16 @@ public class UserResource {
 	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {		
+	public ResponseEntity<List<UserDTO>> findAll() {		
 		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@GetMapping("/{id}") 
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 
 }
--- src/main/java/com/mucheniski/workshopmongo/services/UserService.java
+++ src/main/java/com/mucheniski/workshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package com.mucheniski.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mucheniski.workshopmongo.domains.User;
+import com.mucheniski.workshopmongo.exception.ObjectNotFoundException;
 import com.mucheniski.workshopmongo.repository.UserRepository;
 
 @Service
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado para o id: " + id));		
 	}
 	
 }
