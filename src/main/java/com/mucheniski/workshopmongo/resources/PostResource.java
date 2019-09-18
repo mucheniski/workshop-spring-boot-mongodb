de2ca3af7617614104a82ff7e391d8abe4d16f3a
Diego Mucheniski <mucheniski@gmail.com>
2019-09-18 20:18:29 -0300

    332 Consulta simples com Query Methods

--- src/main/java/com/mucheniski/workshopmongo/resources/PostResource.java
+++ src/main/java/com/mucheniski/workshopmongo/resources/PostResource.java
@@ -1,14 +1,18 @@
 package com.mucheniski.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mucheniski.workshopmongo.domains.Post;
 import com.mucheniski.workshopmongo.services.PostService;
+import com.mucheniski.workshopmongo.util.URL;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -21,6 +25,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@GetMapping("/findByTitleContainingIgnoreCase") 
+	public ResponseEntity<List<Post>> findByTitleContainingIgnoreCase(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decoreParam(text);
+		List<Post> posts = postService.findByTitleContainingIgnoreCase(text);		
+		return ResponseEntity.ok().body(posts);
 	}
 	
 }
--- src/main/java/com/mucheniski/workshopmongo/services/PostService.java
+++ src/main/java/com/mucheniski/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.mucheniski.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> post = postRepository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado para o id: " + id));		
+	}
+	
+	public List<Post> findByTitleContainingIgnoreCase(String text) {
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
